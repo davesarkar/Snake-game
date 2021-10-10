ad239575e1a4b834d9155255ece3d92e447fd1fa
Sarkar Devashish <devashishsarkar54@gmail.com>
2021-10-10 13:12:53 +0530

    created base

--- Snake-game/src/GameFrame.java
+++ Snake-game/src/GameFrame.java
@@ -1,4 +1,17 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+public class GameFrame extends JFrame {
 
-public class GameFrame {
-
+	GameFrame() {
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setTitle("Hungry Snake");
+		
+		ImageIcon im = new ImageIcon("snake.png");
+		
+		this.setIconImage(im.getImage());
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		}
 }
